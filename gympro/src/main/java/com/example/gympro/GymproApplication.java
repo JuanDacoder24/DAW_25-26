fb61453cceb9c695690b03aca342a46175929e9a
JuanDacoder24 <servellonrejasjuandavid@gmail.com>
2025-10-24 12:29:17 +0200

    arreglar el problema de los estilos y las imagenes, hacer las demas paginas pendientes

---
+++ gympro/src/main/java/com/example/gympro/GymproApplication.java
@@ -0,0 +1,13 @@
+package com.example.gympro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymproApplication.class, args);
+	}
+
+}
---
+++ gympro/src/test/java/com/example/gympro/GymproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gympro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
